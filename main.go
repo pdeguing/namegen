766f8f962b3be5564f3dfb0e02647f1027a226e2
pdeguing <pierrequentindeguingand@gmail.com>
2020-04-27 16:14:50 +0200

    tiny cleanup

--- main.go
+++ main.go
@@ -4,15 +4,11 @@ Package namegen implements a simple library to generate random names.
 package namegen
 
 import (
-	"fmt"
 	"math/rand"
 	"strings"
 )
 
 func isVowel(c byte) bool {
-	if c <= 0 {
-		return false
-	}
 	return strings.Contains("aeiouy", string(c))
 }
 
@@ -36,12 +32,15 @@ func Name(r *rand.Rand, length int) (string, error) {
 	if length <= 0 {
 		return "", nil
 	}
+
 	buffer := make([]byte, length)
+
 	if r.Intn(9) < 4 {
 		buffer[0] = getVowel(r)
 	} else {
 		buffer[0] = getConsonant(r)
 	}
+
 	for i := 1; i < length; i++ {
 		if i > 1 {
 			if isVowel(buffer[i-1]) && isVowel(buffer[i-2]) {
@@ -66,20 +65,14 @@ func Name(r *rand.Rand, length int) (string, error) {
 func Nname(r *rand.Rand, min_length, max_length, n int) ([]string, error) {
 	var length int
 	var names	[]string
-	var generated	string
 
 	for i := 0; i < n; i++ {
 		length = r.Intn(max_length-min_length) + min_length
-		generated, _ = Name(r, length)
+		generated, err := Name(r, length)
+		if err != nil {
+			return names, err
+		}
 		names = append(names, generated)
 	}
 	return names, nil
-}
-
-func main() {
-	r := rand.New(rand.NewSource(42))
-	min_length := 3
-	max_length := 9
-	names, _ := Nname(r, min_length, max_length, 50)
-	fmt.Println(names)
 }
--- unit_test.go
+++ unit_test.go
@@ -1,10 +1,23 @@
 package namegen
 
-import "testing"
+import (
+	"testing"
+	"math/rand"
+)
 
 func TestIsVowel(t *testing.T) {
 	got := isVowel(0)
 	if got != false {
 		t.Errorf("isVowel(0) = %t; want false", got)
+	}
+}
+
+func TestNname(t *testing.T) {
+	r := rand.New(rand.NewSource(42))
+	min_length := 3
+	max_length := 9
+	got, err := Nname(r, min_length, max_length, 50)
+	if err != nil {
+		t.Errorf("error while generating 50 names: %e", err)
 	}
 }
